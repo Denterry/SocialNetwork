1fff9b1ed57d4b9ad732731828eb09ac8290e9d4
Denis Todorov <denis6_4@mail.ru>
2024-04-08 21:21:00 +0300

    Rough Version of Post Service

--- postService/cmd/main.go
+++ postService/cmd/main.go
@@ -1,36 +1,76 @@
 package main
 
 import (
-	"log"
-	"net"
+	"log/slog"
+	"os"
 
-	"github.com/Denterry/SocialNetwork/postService/api/proto"
-	"github.com/Denterry/SocialNetwork/postService/internal/storage"
-	postdealer "github.com/Denterry/SocialNetwork/postService/pkg/postDealer"
-	"google.golang.org/grpc"
+	"github.com/Denterry/SocialNetwork/postService/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
-	storage.InitDb()
-	defer storage.Db.Close()
+	cfg := config.MustLoad()
+	// fmt.Println(cfg)
 
-	lis, err := net.Listen("tcp", ":5005")
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
-	}
-
-	grpcServer := grpc.NewServer()
-	srv := &postdealer.GRPCServer{}
-	proto.RegisterPostServiceServer(grpcServer, srv)
-
-	postRepository := repository.NewPostRepository()
-	postService := service.NewPostService(postRepository)
-	postHandler := handler.NewPostHandler(postService)
-
-	pb.RegisterPostServiceServer(grpcServer, postHandler)
-
-	log.Println("gRPC server is running...")
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
-	}
+	log := setupLogger(cfg.Env)
+	log.Info("starting application", slog.Any("config", cfg))
+	// log.Info("starting application",
+	// 	slog.String("env", cfg.Env),
+	// 	slog.Any("cfg", cfg),
+	// 	slog.Int("port", cfg.GRPC.Port),
+	// )
+	// log.Debug("debug message")
+	// log.Error("error message")
+	// log.Warn("warn message")
 }
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+// func main() {
+// 	storage.InitDb()
+// 	defer storage.Db.Close()
+
+// 	lis, err := net.Listen("tcp", ":5005")
+// 	if err != nil {
+// 		log.Fatalf("Failed to listen: %v", err)
+// 	}
+
+// 	grpcServer := grpc.NewServer()
+// 	srv := &postdealer.GRPCServer{}
+// 	proto.RegisterPostServiceServer(grpcServer, srv)
+
+// 	postRepository := repository.NewPostRepository()
+// 	postService := service.NewPostService(postRepository)
+// 	postHandler := handler.NewPostHandler(postService)
+
+// 	pb.RegisterPostServiceServer(grpcServer, postHandler)
+
+// 	log.Println("gRPC server is running...")
+// 	if err := grpcServer.Serve(lis); err != nil {
+// 		log.Fatalf("Failed to serve: %v", err)
+// 	}
+// }
--- postService/go.mod
+++ postService/go.mod
@@ -1,6 +1,6 @@
 module github.com/Denterry/SocialNetwork/postService
 
-go 1.22.1
+go 1.22.2
 
 require (
 	github.com/joho/godotenv v1.5.1
@@ -8,7 +8,9 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
@@ -16,4 +18,6 @@ require (
 	google.golang.org/grpc v1.62.1 // indirect
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
